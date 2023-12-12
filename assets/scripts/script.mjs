15323a21a2efebb7ae7e92acb3fb42b89e5c4c63
Richard Eze <richardeze391@gmail.com>
2023-12-12 11:22:47 +0100

    finally descided on the new user interface, so committing the new change

--- assets/scripts/script.mjs
+++ assets/scripts/script.mjs
@@ -1,13 +1,12 @@
-
-document.addEventListener("DOMContentLoaded", async ()=>{
+/*  document.addEventListener("DOMContentLoaded", async ()=>{
   const {slideshow_img} = await import("./components/ads/slideshow_img/slideshow_img.js")
   slideshow_img()
-})
+})  */
 
-const ads_illustration = document.querySelectorAll(".ads_ilustration");
+/* const ads_illustration = document.querySelectorAll(".ads_ilustration"); */
 
 
-ads_illustration.forEach(illustration =>{
+/* ads_illustration.forEach(illustration =>{
   illustration.addEventListener("mouseover",async ()=>{
     const {slideshow_btn, slideshow_btn_hide} = await import("./components/ads/slideshow_btn/slideshow_btn.js")
     slideshow_btn(illustration)
@@ -19,4 +18,49 @@ ads_illustration.forEach(illustration =>{
     const {slideshow_btn, slideshow_btn_hide} = await import("./components/ads/slideshow_btn/slideshow_btn.js")
     slideshow_btn_hide(illustration)
   })
+}) */
+
+
+/* gsap.set('.cursor',{xPercent:-50, yPercent: -50})
+
+let cursor = document.querySelector('.cursor')
+let hand = document.querySelector('.hand')
+let title = document.querySelector('.banner')
+
+let mouseX;
+let mouseY;
+
+window.addEventListener('mousemove', e => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+
+    gsap.to(cursor, 0.5, {x: mouseX, y:mouseY})
 })
+
+title.addEventListener('mouseenter', () => {
+    gsap.to(hand, 1, {
+        scale: 1,
+        opacity: 1,
+        top: '-75px',
+        left: '-75px',
+        rotate: 0,
+        ease: Elastic.easeOut.config(1, 0.3)
+    })
+})
+
+title.addEventListener('mousemove', () => {
+    gsap.to(hand, 1, {
+        x: mouseX,
+        y: mouseY
+    })
+})
+
+title.addEventListener('mouseleave', () => {
+    gsap.to(hand, 0.2, {
+        scale: 0,
+        opacity: 0,
+        top: '10',
+        left: '40',
+        rotate: 45,
+    })
+}) */
